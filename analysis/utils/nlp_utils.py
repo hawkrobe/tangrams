170e81c20572a2d679f6a9cb1d6dc4e0b3f566c2
Robert Hawkins <rxdh@stanford.edu>
2018-09-02 21:02:54 -0700

    Add function to go through and correct spelling

--- analysis/utils/nlp_utils.py
+++ analysis/utils/nlp_utils.py
@@ -1,145 +1,94 @@
-from nltk.tokenize import word_tokenize
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tree import Tree
+from symspellpy.symspellpy import SymSpell, Verbosity 
+from collections import defaultdict
+from IPython.display import clear_output
+import os 
+import json
 
-LEMMATIZER = WordNetLemmatizer()
-
-def stanford_pos(text):
-    """
-    Parameters
-    ----------
-    text : str
-       CoreNLP handles all tokenizing, at the sentence and word level.
-       
-    Returns
-    -------
-    list of tuples (str, str)
-       The first member of each pair is the word, the second its POS tag.          
-    """
-    try:
-        ann = nlp.annotate(
-            text, 
-            properties={'annotators': 'pos', 
-                        'outputFormat': 'json'})
-        lemmas = []
-        for sentence in ann['sentences']:
-            for token in sentence['tokens']:
-                lemmas.append((token['word'], token['pos']))
-    except Exception as e:
-        print(text + ": cannot parse")
-        lemmas = []
-    return lemmas
+def write_json(d, filename) :
+    with open(filename, 'w') as f:
+        json.dump(d, f)
+        
+def read_json(filename) :
+    with open(filename) as f:
+        return json.load(f)
     
-def stanford_parsetree(text):
-    """
-    Parameters
-    ----------
-    text : str
-       CoreNLP handles all tokenizing, at the sentence and word level.
-       
-    Returns
-    -------
-    list of tuples (str, str)
-       The first member of each pair is the word, the second its POS tag.          
-    """
-    try:
-        ann = nlp.annotate(
-            text, 
-            properties={'annotators': 'parse', 
-                        'outputFormat': 'json'})
-        lemmas = []
-        for sentence in ann['sentences']:
-            lemmas.append(sentence['parse'])
-    except Exception as e:
-        print(text,": cannot parse")
-        lemmas = []
-    return lemmas
+# initial capacity, max_edit_distance_dictionary, prefix_length
+sym_spell = SymSpell(83000, 2, 7)
+dictionary_path = os.path.join("./frequency_dictionary_en_82_765.txt")
+
+term_index = 0  # column of the term in the dictionary text file
+count_index = 1  # column of the term frequency in the dictionary text file
+assert(sym_spell.load_dictionary(dictionary_path, term_index, count_index))
+
+def building_spell_correction_dictionary(game_data, uber_vocabulary, special_symbols):
+    ''' Will search for all words of game_data that are not in the uber_vocabulary neither are special symbols. It will prompt the user with an automated spell-checked proposal and allow him to give an alternative. Also, if an old_dictionary is provided, mispelled words that exist on it will not be prompted to the user.
+    Note: A) For the purposes of the language-3d-context project, if the user is not certain about the speell-correction that must be applied they must provide the token: `<UKN>`. 
+    It is important to not leave a word that is NOT in the uber_vocabulary without converting it into a word that is in the dictionary, or to <UKN>.    
+    B) It is ok to map a misspelled token to multiple ones: 'youthinK -> you think'.
     
-def is_comp_sup(word, pos, tags, check_lemmatizer=False):
-    """
-    Parameters
-    ----------
-    word, pos : str, str
-        The lemma.
+    TODO-J: Force correction to be in uber-dictionary (If many words -> use split() and verify each one is the dictionary).
+    '''
+    old_dictionary = read_json(os.path.join("./spell_correction.json")) 
+
+    missed_words, missed_words_with_context = words_not_in_language(game_data, uber_vocabulary, special_symbols)
+    if old_dictionary is not None:   # Use the old dictionary.
+        missed_words_temp = missed_words.copy()
+        for m in missed_words:
+            if m in old_dictionary:
+                del missed_words_with_context[m]
+                missed_words_temp.remove(m)
+        missed_words = missed_words_temp
     
-    tags : iterable of str
-        The tags considered positive evidence for comp/sup morphology.
-       
-       
-    check_lemmatizer : bool
-        If True, then if the `pos` is in `tags`, we also check that
-        `word` is different from the lemmatized version of word
-        according to WordNet, treating it as an adjective. This 
-        could be used to achieve greater precision, perhaps at the
-        expense of recall.
-       
-    Returns
-    -------
-    bool       
-    """
-    if pos not in tags:
-        return False
-    if check_lemmatizer and LEMMATIZER.lemmatize(word, 'a') == word:
-        return False
-    return True
+    print('Words that neeed spell-check.', len(missed_words))
+    
+    corrector = old_dictionary.copy()
+    for m in missed_words:    
+        print(m, missed_words_with_context[m])
+        max_edit_distance_lookup = 10
+        _s = sym_spell.lookup(m, max_edit_distance_lookup)
+        s = _s[0].term if _s else '<no suggestion>'
+        print(s, s in uber_vocabulary)
+        
+        ans = input('Do you like the proposed correction(y/n)?\n')
+        
+        while ans not in ['n', 'y']:
+            ans = input('Do you like the proposed correction(y/n)?\n')
+        
+        if ans == 'n':
+            while True:
+                ans = input('Give your alternative.\n')                
+                print(ans, 'in uber vocab?:', ans in uber_vocabulary)
+                
+                ok = input('ok (y/n)?\n')
+                clear_output()
+                if ok == 'y':
+                    corrector[m] = ans
+                    write_json(corrector, './spell_correction.json')
+                    break
+        else:
+            corrector[m] = s
+            write_json(corrector, './spell_correction.json')
 
-def is_noun(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'NN', 'NNS', 'NNP', 'NNPS'}, check_lemmatizer=check_lemmatizer)
 
-def is_prep(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'IN'}, check_lemmatizer=check_lemmatizer)
+def words_not_in_language(game_data, language, special_tokens):
+    '''
+    Args:
+        language: dict/set keys of which are the words that form the language model.
+        special_tokens: set with words marking special meaning e.g. <DIA>, <UKN>.
+    Returns:
+        missed_words: missing words.
+        missed_words_with_context: the entire sentece which had the missing word.
+    '''    
+    missed_words = set()
+    missed_words_with_context = defaultdict(list)
+    text = game_data['text']
+    for sentence in text:
+        which = []
+        for word in sentence:      
+            if word.lower_ not in language and word not in special_tokens:                
+                missed_words.add(word.lower_)
+                which.append(word.lower_)
 
-def is_verb(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'MD', 'VB', 'VBZ', 'VBP', 'VBD', 'VBN', 'VBG'}, check_lemmatizer=check_lemmatizer)
-
-def is_det(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'DT', 'WDT'}, check_lemmatizer=check_lemmatizer)
-
-def is_pronoun(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'PRP', 'PRP$', 'WP', 'WP$'}, check_lemmatizer=check_lemmatizer)
-
-def is_adjective(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'JJ', 'JJR', 'JJS'}, check_lemmatizer=check_lemmatizer)
-
-def is_adverb(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'RB', 'RBR', 'RBS', 'RP', 'WRB'}, check_lemmatizer=check_lemmatizer)
-
-def is_num(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'CD'}, check_lemmatizer=check_lemmatizer)
-
-def is_other(word, pos, check_lemmatizer=False):
-    return is_comp_sup(
-        word, pos, {'EX', 'FW', 'LS', 'PDT', 'POS', 'SYM', 'TO', 'UH'}, check_lemmatizer=check_lemmatizer)
-
-def get_np_words(tree):
-    """Finds NP (nounphrase) leaf nodes of a chunk tree."""
-    for subtree in tree.subtrees(filter = lambda t: t.label()=='NP'):
-        yield subtree.leaves()
-  
-def tag_count(treestring, tag) :
-    """Counts occurrences of `tag` in given tree"""
-    count = 0
-    tree = Tree.fromstring(treestring)
-    for subtree in tree.subtrees(filter = lambda t: t.label()==tag):
-        count += 1
-    return count 
-
-def pp_count(treestring) :
-    """Counts PPs in tree"""
-    return tag_count(treestring, 'PP')
-
-def sbar_count(treestring) :
-    """Counts subclauses in tree"""
-    return tag_count(treestring, 'SBAR')
-
-def cc_count(treestring) :
-    """Counts subclauses in tree"""
-    return tag_count(treestring, 'CC')
+        for w in which:
+            missed_words_with_context[w].append(sentence)
+    return missed_words, missed_words_with_context

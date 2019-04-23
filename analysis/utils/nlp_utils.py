33912e29add56491852ce53c03f20689b1386d29
Robert Hawkins <hawkrobe@gmail.com>
2019-04-23 02:47:26 -0700

    generate glove feats in helper

--- analysis/utils/nlp_utils.py
+++ analysis/utils/nlp_utils.py
@@ -1,8 +1,42 @@
-from symspellpy.symspellpy import SymSpell, Verbosity 
+#from symspellpy.symspellpy import SymSpell, Verbosity 
 from collections import defaultdict
 from IPython.display import clear_output
 import os 
 import json
+import numpy as np
+import pandas as pd
+
+def get_feats(d_in, docs_emb, nlp) :
+    d = d_in.drop(d_in.columns.difference(['gameid', 'intendedName', 'repetitionNum', 'text', 'correct']), axis=1)
+    meta = pd.DataFrame(columns = ['gameid',  'intendedName','repetitionNum', 'correct', 'empty'])
+    raw_avg_feats = np.array([]).reshape(0, 300)
+    weighted_feats = np.array([]).reshape(0, 300)
+
+    for i, row in d.iterrows() :
+        null_embedding = np.full((1,300), np.nan)
+
+        # sometimes we have a missing row and we want to put in NANs so it's handled properly when taking means
+        if pd.isna(row['text']) :
+            raw_avg_embedding = null_embedding
+            weighted_embedding = null_embedding
+            is_empty = True
+        else :
+            weighted_embedding = docs_emb[i,]
+            local_embedding = np.array([]).reshape(0, 300)
+            for token in row['text'] :
+                if token.pos_ in ['NOUN', 'VERB', 'ADJ'] and token.has_vector :
+                    local_embedding = np.vstack((local_embedding, nlp.vocab[token.lemma].vector))
+            is_empty = local_embedding.size == 0
+            if is_empty :
+                raw_avg_embedding = null_embedding
+            else :
+                raw_avg_embedding = np.nanmean(local_embedding, axis = 0)
+        new_row = list(row[:4]) + [is_empty]
+        new_df = pd.DataFrame([new_row], columns = ['gameid',  'intendedName', 'repetitionNum', 'correct', 'empty'])
+        meta = meta.append(new_df, ignore_index=True)
+        raw_avg_feats = np.vstack((raw_avg_feats, raw_avg_embedding))
+        weighted_feats = np.vstack((weighted_feats, weighted_embedding))
+    return meta, raw_avg_feats, weighted_feats
 
 def write_json(d, filename) :
     with open(filename, 'w') as f:
@@ -12,14 +46,6 @@ def read_json(filename) :
     with open(filename) as f:
         return json.load(f)
     
-# initial capacity, max_edit_distance_dictionary, prefix_length
-sym_spell = SymSpell(83000, 2, 7)
-dictionary_path = os.path.join("./frequency_dictionary_en_82_765.txt")
-
-term_index = 0  # column of the term in the dictionary text file
-count_index = 1  # column of the term frequency in the dictionary text file
-assert(sym_spell.load_dictionary(dictionary_path, term_index, count_index))
-
 def building_spell_correction_dictionary(game_data, uber_vocabulary, special_symbols):
     ''' Will search for all words of game_data that are not in the uber_vocabulary neither are special symbols. It will prompt the user with an automated spell-checked proposal and allow him to give an alternative. Also, if an old_dictionary is provided, mispelled words that exist on it will not be prompted to the user.
     Note: A) For the purposes of the language-3d-context project, if the user is not certain about the speell-correction that must be applied they must provide the token: `<UKN>`. 
@@ -28,6 +54,14 @@ def building_spell_correction_dictionary(game_data, uber_vocabulary, special_sym
     
     TODO-J: Force correction to be in uber-dictionary (If many words -> use split() and verify each one is the dictionary).
     '''
+    # initial capacity, max_edit_distance_dictionary, prefix_length
+    sym_spell = SymSpell(83000, 2, 7)
+    dictionary_path = os.path.join("./frequency_dictionary_en_82_765.txt")
+
+    term_index = 0  # column of the term in the dictionary text file
+    count_index = 1  # column of the term frequency in the dictionary text file
+    assert(sym_spell.load_dictionary(dictionary_path, term_index, count_index))
+
     old_dictionary = read_json(os.path.join("./spell_correction.json")) 
 
     missed_words, missed_words_with_context = words_not_in_language(game_data, uber_vocabulary, special_symbols)

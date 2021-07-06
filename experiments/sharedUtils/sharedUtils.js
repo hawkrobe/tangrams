216640f52ae04f0c8e55620ef7e31971760825a5
Robert Hawkins <hawkrobe@gmail.com>
2021-07-05 23:58:27 -0700

    remove outdated utils

--- experiments/sharedUtils/sharedUtils.js
+++ experiments/sharedUtils/sharedUtils.js
@@ -38,73 +38,6 @@ function fillArray(value, len) {
   return arr;
 }
 
-var checkInBounds = function(object, options) {
-  return (object.x + (object.w || object.d) < options.width) &&
-         (object.y + (object.h || object.d) < options.height);
-};
-
-var randomColor = function (options) {
-  var h = ~~(Math.random() * 360);
-  var s = ~~(Math.random() * 100);
-  var l = _.has(options, 'fixedL') ? 50 : ~~(Math.random() * 100) ;
-  return [h, s, l];
-};
-
-var randomSpline = function () {
-  var numPoints = 4;
-  return _.sample(_.range(50, 250), 2 * numPoints);
-};
-
-// returns an object with x, y, w, h fields
-var randomRect = function(options) {
-  if (_.isEmpty(options)) {
-    throw "Error, must provide options to randomRect!";
-  }
-
-  var wRange = _.range(options.wMin, options.wMax);
-  var hRange = _.range(options.hMin, options.hMax);
-
-  var rect = randomPoint(options);
-  rect.h = _.sample(wRange),
-  rect.w = _.sample(hRange)
-
-  if (!checkInBounds(rect, options)) {
-    return this.randomRect(options);
-  }
-
-  return rect;
-}
-
-var randomCircle = function(options) {
-  if (_.isEmpty(options)) {
-    throw "Error, must provide options to randomCircle!";
-  }
-
-  //TODO, better error checking
-  var dRange = _.range(options.dMin, options.dMax);
-  if (_.isEmpty(dRange)) dRange = [options.dMin]; //hacky for now
-
-  var circle = randomPoint(options);
-  circle.d = _.sample(dRange);
-
-  if (!checkInBounds(circle, options)) {
-    return this.randomCircle(options);
-  }
-
-  return circle;
-}
-
-var randomPoint = function(options) {
-
-  var xRange = _.range(options.xMin, options.xMax);
-  var yRange = _.range(options.yMin, options.yMax);
-
-  return {
-    x: _.sample(xRange),
-    y: _.sample(yRange)
-  }
-}
-
 // --- below added by jefan March 2017
 // extracts all the values of the javascript dictionary by key
 var vec = function extractEntries(dict,key) {
@@ -152,12 +85,5 @@ module.exports = {
   UUID : UUID,
   getLongFormTime : getLongFormTime,
   establishStream: establishStream,
-  hsl2lab : hsl2lab,
   fillArray: fillArray,
-  randomColor: randomColor,
-  randomRect: randomRect,
-  randomCircle: randomCircle,
-  randomPoint: randomPoint,
-  randomSpline: randomSpline,
-  colorDiff : colorDiff
 };
